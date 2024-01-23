934287d7c8f8862bec9680842aaed2565f7d4cc9
Nome <miranhaomi@gmail.com>
2024-01-23 19:05:31 -0300

    Criação dos repositorios e das migrations para o banco de dados

--- credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/domain/Credit.kt
+++ credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/domain/Credit.kt
@@ -1,16 +1,19 @@
 package me.dio.creditapplicationsystem.domain
 
+import jakarta.persistence.*
 import me.dio.creditapplicationsystem.enummeration.Status
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.*
 
+@Entity
+@Table(name = "Credito")
 data class Credit (
-    val creditCode: UUID = UUID.randomUUID(),
-    val creditValue: BigDecimal = BigDecimal.ZERO,
-    val dayFirstIntallment: LocalDate,
-    val numberOfInstallments: Int = 0,
-    val status: Status = Status.IN_PROGRESS,
-    val customer: Customer? = null,
-    val id: Long? = null
+    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) val dayFirstIntallment: LocalDate,
+    @Column(nullable = false) val numberOfInstallments: Int = 0,
+    @Enumerated val status: Status = Status.IN_PROGRESS,
+    @ManyToOne val customer: Customer? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
